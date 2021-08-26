8fee78464223fb49d68960316134a162ac0b5b3c
Oksana Terekhova <trose@yandex.ru>
2021-08-26 13:48:14 +0300

    implement HW_5 part 2

--- src/com/basejava/webapp/storage/MapStorage.java
+++ src/com/basejava/webapp/storage/MapStorage.java
@@ -7,11 +7,12 @@ import java.util.HashMap;
 public class MapStorage extends AbstractStorage {
     HashMap<String, Resume> map = new HashMap<>();
 
-
+    @Override
     public Resume[] getAll() {
         return map.values().toArray(new Resume[0]);
     }
 
+    @Override
     protected int findIndex(String uuid) {
         if (!map.containsKey(uuid)) {
             return -1;
@@ -19,26 +20,32 @@ public class MapStorage extends AbstractStorage {
         return 1;
     }
 
+    @Override
     protected void deleteResume(int index, String uuid) {
         map.remove(uuid);
     }
 
+    @Override
     protected void saveResume(Resume resume, int index) {
         map.put(resume.getUuid(), resume);
     }
 
+    @Override
     protected void updateResume(Resume resume, int index) {
         map.put(resume.getUuid(), resume);
     }
 
+    @Override
     public int size() {
         return map.size();
     }
 
+    @Override
     public void clear() {
         map.clear();
     }
 
+    @Override
     protected Resume getResume(int index, String uuid) {
         return map.get(uuid);
     }
